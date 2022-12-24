b9c0d0867fc537176daeb1e52235fc12ede24232
Augusto.9237 <augusto.souza8330@gmail.com>
2022-12-24 18:39:19 -0300

    sigin ok

--- src/components/SigIn/styles.ts
+++ src/components/SigIn/styles.ts
@@ -1,140 +1,140 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100vh;
+flex: 1;
+max-width: 1216px;
+margin: auto;
+
+@media (max-width: 860px) {
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  flex: 1;
-  max-width: 1216px;
-  margin: auto;
+  padding: 0 40px;
+}
 
-  @media (max-width: 860px) {
-    flex-direction: column;
-    padding: 0 40px;
-  }
+@media (max-width: 768px) {
+  flex-direction: column;
+  padding: 0 30px;
+}
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-    padding: 0 30px;
-  }
-
-  @media (max-width: 415px) {
-    padding: 0 20px;
-  }
+@media (max-width: 415px) {
+  padding: 0 20px;
+}
 `;
 
 export const SigInBody = styled.div`
-  display: flex;
-  width: 100%;
-  height: 500px;
-  max-width: 1060px;
-  border: 1px solid rgba(204, 204, 204, 0.4);
-  border-radius: 16px;
-  overflow: hidden;
+display: flex;
+width: 100%;
+height: 500px;
+max-width: 1060px;
+border: 1px solid rgba(204, 204, 204, 0.4);
+border-radius: 16px;
+overflow: hidden;
 `;
 
 export const ImageContainer = styled.div`
-  background: #d73035;
-  display: flex;
-  height: 100%;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: 42px;
+background: #d73035;
+display: flex;
+height: 100%;
+flex: 1;
+align-items: center;
+justify-content: center;
+padding: 42px;
 
-  img {
-    width: 100%;
-  }
+img {
+  width: 100%;
+}
 
-  @media (max-width: 700px) {
-    display: none;
-  }
+@media (max-width: 700px) {
+  display: none;
+}
 `;
 
 export const SigInContent = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  justify-content: space-around;
-  background: #ffffff;
-  padding: 32px;
+display: flex;
+flex: 1;
+flex-direction: column;
+justify-content: space-around;
+background: #ffffff;
+padding: 32px;
 
-  @media (max-width: 700px) {
-    padding: 0;
-  }
+@media (max-width: 700px) {
+  padding: 0;
+}
 `;
 
 export const SigInHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+@media (max-width: 700px) {
+  background: #d73035;
+  padding: 18px;
+  gap: 12px;
+}
+
+h1 {
+  font-weight: 600;
+  font-size: 24px;
+  color: #333333;
 
   @media (max-width: 700px) {
-    background: #d73035;
-    padding: 18px;
-    gap: 12px;
+    color: #ffffff;
   }
+}
 
-  h1 {
-    font-weight: 600;
-    font-size: 24px;
-    color: #333333;
+img {
+  display: none;
 
-    @media (max-width: 700px) {
-      color: #ffffff;
-    }
+  @media (max-width: 700px) {
+    display: flex;
+    width: 300px;
   }
-
-  img {
-    display: none;
-
-    @media (max-width: 700px) {
-      display: flex;
-      width: 300px;
-    }
-  }
+}
 `;
 
 export const SigInInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 24px 0;
+display: flex;
+flex-direction: column;
+margin: 24px 0;
 
-  span {
-    font-weight: 500;
-    font-size: 14px;
-    margin-bottom: 6px;
-    color: #333333;
-    opacity: 0.9;
+span {
+  font-weight: 500;
+  font-size: 14px;
+  margin-bottom: 6px;
+  color: #333333;
+  opacity: 0.9;
 
-    @media (max-width: 700px) {
-      margin-left: 24px;
-    }
+  @media (max-width: 700px) {
+    margin-left: 24px;
   }
+}
 
-  input {
-    padding: 12px;
-    border: 1px solid rgba(204, 204, 204, 0.4);
-    border-radius: 8px;
+input {
+  padding: 12px;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 8px;
+  margin-bottom: 12px;
+
+  @media (max-width: 700px) {
+    margin: 0 24px;
     margin-bottom: 12px;
-
-    @media (max-width: 700px) {
-      margin: 0 24px;
-      margin-bottom: 12px;
-    }
   }
+}
 `;
 
 export const FooterSigIn = styled.footer`
-   width: 100%;
-   display: flex;
+ width: 100%;
+ display: flex;
 
-   @media (max-width: 700px) {
-     padding: 0 24px;
-     margin-bottom: 24px;
-    }
+ @media (max-width: 700px) {
+   padding: 0 24px;
+   margin-bottom: 24px;
+  }
 `;
