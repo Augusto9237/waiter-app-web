b966d76f4b601c4b21172b78eead8be111388dc6
Augusto Sousa <augusto.souza8330@gmail.com>
2023-01-18 21:01:26 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,64 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-import { AdminPage } from "./Pages/AdminPage";
-import { ClientPage } from "./Pages/Client";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { SigInPage } from "./Pages/SigIn";
-import { OrdersPage } from "./Pages/OrdersPage";
-import { Orders } from "./components/Orders";
-import { Menu } from "./components/Admin/Menu";
-import { Users } from "./components/Admin/Users";
-import Dashboard from "./components/Admin/Dashboard";
-import { GlobalStyles} from "./styles/GlobalStyles";
-
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SigInPage />,
-  },
-  {
-    path: "/orders",
-    element: <OrdersPage />,
-  },
-  {
-    path: "/admin",
-    element: <AdminPage />,
-    children: [
-      {
-        path: "",
-        element: <Dashboard />,
-      },
-      {
-        path: "orders",
-        element: <Orders />,
-      },
-      {
-        path: "menu",
-        element: <Menu />,
-      },
-      {
-        path: "users",
-        element: <Users />,
-      }
-    ]
-  },
-  {
-    path: "/client/:tableNumber",
-    element: <ClientPage />,
-  },
-]);
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
-    <GlobalStyles />
-    <RouterProvider router={router} />
+    <Routes/>
   </>
   //  </React.StrictMode>
 );
---
+++ src/routes.tsx
@@ -0,0 +1,54 @@
+import { Users } from "phosphor-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./components/Admin/Dashboard";
+import { Menu } from "./components/Admin/Menu";
+import { Orders } from "./components/Orders";
+import { AdminPage } from "./Pages/AdminPage";
+import { ClientPage } from "./Pages/Client";
+import { OrdersPage } from "./Pages/OrdersPage";
+import { SigInPage } from "./Pages/SigIn";
+
+
+export function Routes() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <SigInPage />,
+        },
+        {
+            path: "/orders",
+            element: <OrdersPage />,
+        },
+        {
+            path: "/admin",
+            element: <AdminPage />,
+            children: [
+                {
+                    path: "",
+                    element: <Dashboard />,
+                },
+                {
+                    path: "orders",
+                    element: <Orders />,
+                },
+                {
+                    path: "menu",
+                    element: <Menu />,
+                },
+                {
+                    path: "users",
+                    element: <Users />,
+                }
+            ]
+        },
+        {
+            path: "/client/:tableNumber",
+            element: <ClientPage />,
+        },
+    ]);
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+}
