f259a3b0c6e6984a16dd822335295adc5fbd1ae0
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-01-20 12:58:20 +0000

    theme context in progress

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,3 +1,10 @@
 import { createContext } from 'react';
+import { ThemeTypes } from '../types/Theme';
+
+interface ThemeContextProps {
+    lightTheme: ThemeTypes;
+    darkTheme: ThemeTypes;
+    toggleTheme: () => void;
+  }
 
 export const ThemeContext = createContext<'light' | 'dark'>('light');
---
+++ src/types/Theme.ts
@@ -0,0 +1,11 @@
+export interface ThemeTypes {
+    colors: {
+        primary: string;
+        secondary: string;
+        background: string;
+        border: string;
+        text: string;
+        buttonPrimary: string;
+        buttonSecondary: string;
+    };
+}
