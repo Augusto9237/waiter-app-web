a46c6261d08c45b8859ff14a556de88d42ffdec0
Augusto.9237 <augusto.souza8330@gmail.com>
2023-01-19 23:00:22 -0300

    theme update

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,19 +1,19 @@
 export const lightTheme = {
-    colors: {
-      primary: '#FFFFFF',
-      secondary: '#CCCCCC',
-      background: '#F2F2F2',
-      border: '#D9DDE4',
-      text: '#333333',
-    },
-  };
-  
-  export const darkTheme = {
-    colors: {
-      primary: '#202328',
-      secondary: '#31363F',
-      background: '#101114',
-      border: '#31363F',
-      text: '#FFFFFF',
-    },
-  };
+  colors: {
+    primary: "#FFFFFF",
+    secondary: "#F3F3F3",
+    background: "#E6E6E6",
+    border: "#D9DDE4",
+    text: "#333333",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    primary: "#202328",
+    secondary: "#31363F",
+    background: "#101114",
+    border: "#31363F",
+    text: "#FFFFFF",
+  },
+};
