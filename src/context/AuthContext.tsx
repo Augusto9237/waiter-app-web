6c78f7219f581ee15a5e9d5a4992a0f1e2e9f956
Augusto.9237 <augusto.souza8330@gmail.com>
2023-01-25 01:37:09 -0300

    login in progress

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,15 @@
 import { createContext } from "react";
 import { UserType } from "../types/Users";
 
+export type UserDecode = {
+    iat: number;
+    id: string;
+    nm: string;
+    of: string;
+}
+
 export type AuthContextType = {
-    user: UserType | null;
+    user: UserDecode | unknown;
     signin: (name: string, password: string) => Promise<boolean>;
     signout: () => void;
 }
--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,21 +1,47 @@
-import { ReactNode, useState } from "react";
-import { AuthContext } from "./AuthContext";
-import { UserType } from "../types/Users";
+import { ReactNode, useEffect, useState } from "react";
+import { AuthContext, UserDecode } from "./AuthContext";
 import { useAuth } from "./useAuth";
+import jwtDecode from "jwt-decode";
+import { api } from "../utils/api";
+
 
 interface AuthProps {
     children: ReactNode;
 }
 
 export const AuthProvider = ({ children }: AuthProps) => {
-    const [user, setUser] = useState<UserType | null>(null);
-    const [token, setToken] = useState('');
+    const [user, setUser] = useState<UserDecode | unknown>(null);
+    const [loading, setLoading] = useState(false);
     const auth = useAuth();
+
+
+
+    useEffect(() => {
+        const recoveredUser = localStorage.getItem("u");
+        const token = localStorage.getItem("tkn");
+
+        if (recoveredUser && token) {
+            setUser(JSON.parse(recoveredUser));
+            api.defaults.headers.Authorization = `Bearer ${token}`;
+          }
+        
+        setLoading(false);
+    }, []);
+
+
 
     async function signin(name: string, password: string) {
         const data = await auth.signin(name, password);
-        if (data.token) {
-            setToken(data.token);
+        const decode = jwtDecode(data.token);
+
+        if (data.token && decode) {
+            setUser(decode);
+
+            api.defaults.headers.Authorization = `Bearer ${data.token}`;
+
+            localStorage.setItem("u", JSON.stringify(decode));
+            localStorage.setItem("tkn", data.token);
+
             return true;
         }
         return false;
@@ -26,7 +52,7 @@ export const AuthProvider = ({ children }: AuthProps) => {
     }
 
     return (
-        <AuthContext.Provider value={{ user, signin, signout }}>
+        <AuthContext.Provider value={{ user, signin, signout, }}>
             {children}
         </AuthContext.Provider>
     );
